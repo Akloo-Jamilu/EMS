789454d2be2547bc7d4b5db0cd894707ad294419
Akloo-jamilu <jhasleg126@yahoo.com>
2023-11-10 07:30:58 +0100

    API Routes for employees

--- src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -5,11 +5,9 @@ import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -18,10 +16,24 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-//    Build add employee REST API
+    // Add employee to DB REST API
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto saveEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(saveEmployee, HttpStatus.CREATED);
+    }
+
+    // Get a single employee from DB REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getSingleEmployee(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+//    Get all employees from DB REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
     }
 }
--- src/main/java/net/javaguides/ems/service/implimentation/EmployeeServiceImplimentation.java
+++ src/main/java/net/javaguides/ems/service/implimentation/EmployeeServiceImplimentation.java
@@ -3,10 +3,14 @@ package net.javaguides.ems.service.implimentation;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.exception.ResourceNotFoundException;
 import net.javaguides.ems.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import net.javaguides.ems.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,5 +22,20 @@ public class EmployeeServiceImplimentation implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee saveEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(saveEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee is not found with the given ID" + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+         List<Employee> employees = employeeRepository.findAll();
+         return employees.stream().map((employee)->EmployeeMapper.mapToEmployeeDto(employee))
+                 .collect(Collectors.toList());
+
     }
 }
